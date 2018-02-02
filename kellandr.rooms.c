8dd6ea40db0ddc93ddc8947456fca7412767c2a2
Andrius Kelly <kellandr@os1.engr.oregonstate.edu>
2018-02-01 17:15:37 -0800

    initial commit

---
+++ kellandr.rooms.c
@@ -0,0 +1,32 @@
+/**
+** 	Author:		Andrius Kelly
+** 	Date:		February 2, 2018
+** 	Description:	CS 344 Project 2. This file generates 7 random room files in a directory kellandr.rooms.<process>, to be used by kellandr.adventure.c
+**/
+
+#include <string.h>
+#include <stdio.h>
+
+int main (void) {
+
+	char* roomNames[10];
+
+	//hardcoded room names
+	roomName[0] = "Gatehouse";
+	roomName[1] = "Keep";
+	roomName[2] = "Dungeon";
+	roomName[3] = "Watchtower";
+	roomName[4] = "Turret";
+	roomName[5] = "Sewers";
+	roomName[6] = "Throneroom";
+	roomName[7] = "Courtyard";
+	roomName[8] = "Kitchen";
+	roomName[9] = "Vault";
+
+	char* startRoom = roomName[0];
+	char* endRoom = roomName[9];
+
+
+
+	return 0;
+}
